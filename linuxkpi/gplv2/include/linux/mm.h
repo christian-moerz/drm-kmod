05cfc91f22e14cc9cf6a5b314493c12e256f3a60
Christian Moerz <freebsd@ny-central.org>
2022-12-23 18:17:09 +0100

    damn it

--- linuxkpi/gplv2/include/linux/mm.h
+++ linuxkpi/gplv2/include/linux/mm.h
@@ -3,7 +3,7 @@
 
 #include_next <linux/mm.h>
 
-static inline bool is_cow_mapping(vm_flags_t flags)
+static inline bool is_cow_mapping(unsigned long flags)
 {
 	return (flags & (VM_SHARED | VM_MAYWRITE)) == VM_MAYWRITE;
 }
